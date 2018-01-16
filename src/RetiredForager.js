9daedbf97f03936cfcd641e33974fc9e18a4cb3e
edgarmunozcandia <edgarmunozcandia@gmail.com>
2018-01-15 19:50:53 -0600

    finished beesbeesbees

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,14 @@
 var RetiredForagerBee = function() {
+  RetiredForagerBee.call(this);
+  this.age = 40;
+  this.job = "gamble";
+  this.canFly = "false";
+  this.color = "grey";
+  RetiredForager.prototype.forage = function ("I am too old, let me play cards instead"){
 };
-
+RetiredForager.prototype.gamble = function () {
+  this.treasureChest.push(treasure);
+};
+};
+RetiredForager.prototype = Object.create(Bee.prototype);
+RetiredForager.prototype.constructor = RetiredForager;
